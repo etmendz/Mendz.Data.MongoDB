d89766469be432ea1bbc44d937f6668e98107a76
Edmundo Mendiola <e_mendz@outlook.com>
2017-10-12 11:07:24 -0400

    Provides a generic Mendz.Data-aware context for MongoDB.

---
+++ Mendz.Data.MongoDB/MongoDbDataContext.cs
@@ -0,0 +1,40 @@
+﻿using Mendz.Data.Common;
+using MongoDB.Driver;
+using System;
+
+namespace Mendz.Data.MongoDB
+{
+    /// <summary>
+    /// Provides the database context for a MongoDB database.
+    /// </summary>
+    public class MongoDbDataContext : GenericDbDataContextBase<IMongoDatabase>, IDisposable
+    {
+        protected override IMongoDatabase BuildContext()
+        {
+            var client = new MongoClient(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Client]);
+            return client.GetDatabase(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Context]);
+        }
+
+        #region IDisposable Support
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (_context != null)
+                    {
+                        //_context.Dispose();
+                        _context = null;
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+        #endregion
+    }
+}
---
+++ Mendz.Data.MongoDB/MongoDbDataSettingOption.cs
@@ -0,0 +1,9 @@
+﻿namespace Mendz.Data.MongoDB
+{
+    public static class MongoDbDataSettingOption
+    {
+        public static string Client { get; set;  } = "MongoDBClient";
+
+        public static string Context { get; set; }  = "MongoDBContext";
+    }
+}
