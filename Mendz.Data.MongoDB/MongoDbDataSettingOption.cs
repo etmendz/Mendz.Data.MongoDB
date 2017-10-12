d7edd5db49b0d05639cd7e8c9b799234c2d93ccb
Edmundo Mendiola <e_mendz@outlook.com>
2017-10-12 18:41:55 -0400

    Added documentation to MongoDbDataSettingOption.

--- Mendz.Data.MongoDB/MongoDbDataSettingOption.cs
+++ Mendz.Data.MongoDB/MongoDbDataSettingOption.cs
@@ -1,9 +1,18 @@
 ﻿namespace Mendz.Data.MongoDB
 {
+    /// <summary>
+    /// Provides the data setting options for MongoDB access.
+    /// </summary>
     public static class MongoDbDataSettingOption
     {
+        /// <summary>
+        /// Gets or sets the client name.
+        /// </summary>
         public static string Client { get; set;  } = "MongoDBClient";
 
+        /// <summary>
+        /// Gets or sets the context name.
+        /// </summary>
         public static string Context { get; set; }  = "MongoDBContext";
     }
 }
