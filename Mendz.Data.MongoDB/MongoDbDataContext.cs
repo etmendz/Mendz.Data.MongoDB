fad59ccf32c6ce8daf5c20388258056d7f66dd01
Mendz <e_mendz@hotmail.com>
2018-10-12 13:26:04 -0400

    Added settings, sessions/transactions and options support.

--- Mendz.Data.MongoDB/MongoDbDataContext.cs
+++ Mendz.Data.MongoDB/MongoDbDataContext.cs
@@ -1,6 +1,7 @@
 ﻿using Mendz.Data.Common;
 using MongoDB.Driver;
 using System;
+using System.Threading;
 
 namespace Mendz.Data.MongoDB
 {
@@ -11,9 +12,117 @@ namespace Mendz.Data.MongoDB
     {
         protected override IMongoDatabase BuildContext()
         {
-            var client = new MongoClient(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Client]);
-            return client.GetDatabase(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Context]);
+            MongoClient client;
+            if (MongoDbDataSettingOption.ClientSettings != null)
+            {
+                client = new MongoClient(MongoDbDataSettingOption.ClientSettings);
+            }
+            else
+            {
+                client = new MongoClient(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Client]);
+            }
+            return client.GetDatabase(DataSettingOptions.ConnectionStrings[MongoDbDataSettingOption.Context], MongoDbDataSettingOption.DatabaseSettings);
         }
+
+        #region Session/Transaction Support
+        protected IClientSessionHandle _session = null;
+        /// <summary>
+        /// Gets the client session handle.
+        /// </summary>
+        /// <remarks>
+        /// MongoDB's .Net client/driver is proprietary, with a unique session/transaction design.
+        /// Intentionally, MongoDB's .Net client/driver is not ADO.Net compliant.
+        /// Therefore, IDbDataTransaction is incompatible and cannot be implemented/used.
+        /// This implemention basically aligns with MongoDB's APIs for sessions/transactions.
+        /// However, note that the basic essence and intent of Mendz.Data contexts remains intact.
+        /// </remarks>
+        public IClientSessionHandle Session
+        {
+            get => _session;
+        }
+
+        /// <summary>
+        /// Begins a transaction.
+        /// </summary>
+        /// <param name="clientSessionOptions">Optional client session options.</param>
+        /// <param name="transactionOptions">Optional transaction options.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        public void BeginTransaction(ClientSessionOptions clientSessionOptions = null, TransactionOptions transactionOptions = null, CancellationToken cancellationToken = default)
+        {
+            if (_session == null)
+            {
+                CreateContext();
+                _session = _context.Client.StartSession(clientSessionOptions, cancellationToken);
+                _session.StartTransaction(transactionOptions);
+            }
+        }
+
+        /// <summary>
+        /// Ends a transaction.
+        /// </summary>
+        /// <param name="mode">The transaction mode to commit or rollback.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        public void EndTransaction(EndTransactionMode mode = EndTransactionMode.Commit, CancellationToken cancellationToken = default)
+        {
+            if (_session != null)
+            {
+                if (_session.IsInTransaction)
+                {
+                    if (mode == EndTransactionMode.Commit)
+                    {
+                        _session.CommitTransaction(cancellationToken);
+                    }
+                    else
+                    {
+                        _session.AbortTransaction(cancellationToken);
+                    }
+                }
+                _session.Dispose();
+                _session = null;
+            }
+        }
+
+        /// <summary>
+        /// Begins a transaction asynchronously.
+        /// </summary>
+        /// <param name="clientSessionOptions">Optional client session options.</param>
+        /// <param name="transactionOptions">Optional transaction options.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        public async void BeginTransactionAsync(ClientSessionOptions clientSessionOptions = null, TransactionOptions transactionOptions = null, CancellationToken cancellationToken = default)
+        {
+            if (_session == null)
+            {
+                CreateContext();
+                _session = await _context.Client.StartSessionAsync(clientSessionOptions, cancellationToken);
+                _session.StartTransaction(transactionOptions);
+            }
+        }
+
+        /// <summary>
+        /// Ends a transaction asynchronously.
+        /// </summary>
+        /// <param name="mode">The transaction mode to commit or rollback.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        public async void EndTransactionAsync(EndTransactionMode mode = EndTransactionMode.Commit, CancellationToken cancellationToken = default)
+        {
+            if (_session != null)
+            {
+                if (_session.IsInTransaction)
+                {
+                    if (mode == EndTransactionMode.Commit)
+                    {
+                        await _session.CommitTransactionAsync(cancellationToken);
+                    }
+                    else
+                    {
+                        await _session.AbortTransactionAsync(cancellationToken);
+                    }
+                }
+                _session.Dispose();
+                _session = null;
+            }
+        }
+        #endregion
 
         #region IDisposable Support
         private bool disposed = false;
@@ -24,6 +133,10 @@ namespace Mendz.Data.MongoDB
             {
                 if (disposing)
                 {
+                    if (_session != null)
+                    {
+                        _session.Dispose();
+                    }
                     if (_context != null)
                     {
                         //_context.Dispose();
--- Mendz.Data.MongoDB/MongoDbDataSettingOption.cs
+++ Mendz.Data.MongoDB/MongoDbDataSettingOption.cs
@@ -1,10 +1,22 @@
-﻿namespace Mendz.Data.MongoDB
+﻿using MongoDB.Driver;
+
+namespace Mendz.Data.MongoDB
 {
     /// <summary>
     /// Provides the data setting options for MongoDB access.
     /// </summary>
     public static class MongoDbDataSettingOption
     {
+        /// <summary>
+        /// Gets or sets the client settings. When set/valued, this overrides Client.
+        /// </summary>
+        /// <remarks>
+        /// It is recommended that this property is set at application startup
+        /// via class library/assembly that returns a MongoClientSettings instance.
+        /// This can help hide security sensitive data from the application code.
+        /// </remarks>
+        public static MongoClientSettings ClientSettings { get; set; } = null;
+
         /// <summary>
         /// Gets or sets the client name.
         /// </summary>
@@ -14,5 +26,15 @@
         /// Gets or sets the context name.
         /// </summary>
         public static string Context { get; set; }  = "MongoDBContext";
+
+        /// <summary>
+        /// Gets or sets the database settings.
+        /// </summary>
+        /// <remarks>
+        /// It is recommended that this property is set at application startup
+        /// via class library/assembly that returns a MongoDatabaseSettings instance.
+        /// This can help hide security sensitive data from the application code.
+        /// </remarks>
+        public static MongoDatabaseSettings DatabaseSettings { get; set; } = null;
     }
 }
